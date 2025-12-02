c36bf3c918d5724c3d5140db837830be57453f0f
Nikita Dancik <nmdickens@student.stcc.edu>
2025-12-02 18:52:34 -0500

    Add files via upload

--- Code_Temperature_Converter.py
+++ Code_Temperature_Converter.py
@@ -5,31 +5,35 @@
 sName = input("What is you name: ")
 
 print("Good morning", sName, "My name is Nikita.")
+print("Here is my Temperature Converter")
 
 sTemp = float( input("What is the temperature:  "))
 
 sTemp1 = input("Is the temperature in F for Fahrenheit or C for Celsius: ")
 
+
 # Compute and Output
 if sTemp1 == "F":
-    f_Celsius = format((5.0/9)*(sTemp - 32),'.1f')
-    print("The Celsius equivalent is:") + f_Celsius 
+    f_Celsius = float(format((5.0/9)*(sTemp - 32),'.1f'))
+    print("The Celsius equivalent is:", f_Celsius)
 
-else:
-    print("Must enter a F or C")
-
-raise SystemExit
+    if f_Celsius >= 100:
+        print("Temp can not be > 100")
+    raise SystemExit
     
 if sTemp1 == "C":
-    f_Fahrenheit = format((9.0/5.0)* sTemp + 32,'.1f')
-    print("The Fahrenheit equivalent is:") + f_Fahrenheit
+     f_Fahrenheit = float(format((9.0/5.0)* sTemp + 32,'.1f'))
+     print("The Fahrenheit equivalent is:", f_Fahrenheit)
 
-else:
- print("Must enter a F or C")
- 
-raise SystemExit
+     if f_Fahrenheit >= 212:
+         print("Temp can not > 212")
+         raise SystemExit
+
+if():
+    print("Must enter a F or C")
 
 print("exiting")
+raise SystemExit
 
 
 
